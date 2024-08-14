09510ec12d3daf29379ea14ac36f13060afdf4dd
fgps123 <guilhermesilva2022.gs@gmail.com>
2024-08-14 08:10:25 -0300

    Update preloadAssets.js

--- preloadAssets.js
+++ preloadAssets.js
@@ -4,18 +4,18 @@ function preloadAssets(scene) {
   console.log(scene);
 
   /* Cenário */
-  scene.load.image("fundo", "./fundo.png");
-  scene.load.image("plat", "./plataforma.png");
+  scene.load.image("fundo", "fundo.png");
+  scene.load.image("plat", "plataforma.png");
 
   /* Player */
-  scene.load.spritesheet("player", "./player.png", {
+  scene.load.spritesheet("player", "player.png", {
     frameWidth: 32,
     frameHeight: 48,
   });
 
   /* Coletável */
-  scene.load.image("star", "./star.png");
+  scene.load.image("star", "star.png");
 
   /* Inimigos: bombs */
-  scene.load.image("bombs", "./bomb.png");
+  scene.load.image("bombs", "bomb.png");
 }
